8dca5968f2292228be583f200fd9915cacd3cca7
Daniela <danielacardona2203@gmail.com>
2023-05-19 22:44:48 +0200

    Implement active menu switcher functionality on navigation bar

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+//ACTIVE MENU SWITCHER
+
+const navList = document.querySelector(".nav-list");
+navList.addEventListener("click", (e) => {
+  const navLink = e.target.parentElement;
+  if (navLink.classList.contains("link")){
+    navList.querySelector(".active").classList.remove("active");
+    navLink.classList.add("active");
+  }
+});
