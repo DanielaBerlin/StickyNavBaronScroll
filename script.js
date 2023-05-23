b18a44e9b5a282d485df7449ed8f0743dffc41bb
Daniela <danielacardona2203@gmail.com>
2023-05-23 23:08:32 +0200

    Refactor JavaScript code to support the updated mobile menu design

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const links = document.querySelectorAll('.nav-list li a');
 
-for (link of links) {
+for (const link of links) {
   link.addEventListener('click', smoothScroll);
 }
 
@@ -57,6 +57,29 @@ function scrollToTop() {
 
 // RESPONSIVE MOBILE MENU
 
+// const menu = document.querySelector(".nav-list");
+// const hamburger = document.querySelector(".hamburger");
+// const close = document.querySelector(".close");
+
+// const showMenu = () => {
+//   hamburger.style.display = "none";
+//   close.style.transform = "translateY(0)";
+//   menu.style.transform = "translateY(0)";
+// };
+
+// const hideMenu = () => {
+//   close.style.transform = "translateY(-20rem)";
+//   hamburger.style.display = "block";
+//   menu.style.transform = "translateY(-200%)";
+// }
+
+// hamburger.addEventListener("click", showMenu);
+// close.addEventListener("click", hideMenu);
+
+
+//NEW RESPONSIVE MOBILE MENU
+
+const menuWrapper = document.querySelector(".nav-wrapper");
 const menu = document.querySelector(".nav-list");
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
@@ -64,16 +87,18 @@ const close = document.querySelector(".close");
 const showMenu = () => {
   hamburger.style.display = "none";
   close.style.transform = "translateY(0)";
-  menu.style.transform = "translateY(0)";
+  menuWrapper.style.transform = "translateX(0)";
+  menu.style.transform = "translateX(0)";
 };
 
 const hideMenu = () => {
   close.style.transform = "translateY(-20rem)";
   hamburger.style.display = "block";
-  menu.style.transform = "translateY(-200%)";
+  menuWrapper.style.transform = "translateX(-200)";
+  menu.style.transform = "translateX(200%)";
 }
 
 hamburger.addEventListener("click", showMenu);
 close.addEventListener("click", hideMenu);
-
+menuWrapper.addEventListener("click", hideMenu);
 
